e808e3405a26dce3984b2a674ea351eface18688
John Saputo <jsaputo1@gmail.com>
2020-06-16 01:51:33 +0000

    created ajax request, converted to query string

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,33 +6,33 @@
 
 // Test / driver code (temporary). Eventually will get this from the server.
 
-$(document).ready(function () {
-  // document ready
-
-  const tweetData = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text:
-          "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
+//Tweet database
+const tweetData = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
     },
-  ];
+    content: {
+      text:
+        "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+];
 
-  const renderTweets = (array) => {
-    for (const tweet in array) {
-      // const tweet = createTweetElement(tweet);
-      $(".tweet").append(createTweetElement(tweet));
-    }
-  };
+//Function to render tweets to the .tweets section
+const renderTweets = (array) => {
+  for (const tweet in array) {
+    // const tweet = createTweetElement(tweet);
+    $(".tweet").append(createTweetElement(tweet));
+  }
+};
 
-  const createTweetElement = function (tweet) {
-    return $(`
+//Function to add tweets
+const createTweetElement = function (tweet) {
+  return $(`
     <div class="tweet-container">
       <div class="tweet-header">
       <h3>John Doe</h3>
@@ -49,9 +49,22 @@ $(document).ready(function () {
        
     
     `);
-  };
+};
 
-  //Call function to render tweets
+// renderTweets(tweetData);
 
-  renderTweets(tweetData);
+$(function () {
+  const $form = $("form");
+  $form.on("submit", function () {
+    event.preventDefault();
+    console.log("Button clicked, performing ajax call...");
+    const queryString = $(this).serialize();
+    console.log(queryString);
+    $.ajax("/tweets/", { method: "POST", data: queryString }).then(function (
+      tweets
+    ) {
+      console.log("Success: ", tweets);
+      renderTweets(tweetData);
+    });
+  });
 });
