45301c9d50e8e763f779d0443eeaa0747cef5015
John Saputo <jsaputo1@gmail.com>
2020-06-16 02:31:32 +0000

    created get request to fetch tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -22,36 +22,30 @@ const tweetData = [
   },
 ];
 
-//Function to render tweets to the .tweets section
 const renderTweets = (array) => {
-  for (const tweet in array) {
+  for (const tweet of array) {
     // const tweet = createTweetElement(tweet);
     $(".tweet").append(createTweetElement(tweet));
   }
 };
 
-//Function to add tweets
-const createTweetElement = function (tweet) {
-  return $(`
+const createTweetElement = function (tweetData) {
+  const tweet = `
     <div class="tweet-container">
       <div class="tweet-header">
-      <h3>John Doe</h3>
-      <h3 class="username">@JohnDoe</h3>
+      <h3>${tweetData.user.name}</h3>
+      <h3 class="username">${tweetData.user.handle}</h3>
     </div>
     <article>
-      Hello World!
+    ${tweetData.content.text}
     </article>
-    <div class="tweet-footer">
-      <h4>10 days ago</h4>
+    <div class="tweet-footer"><h4>${tweetData.created_at}</h4>
       <h4>Retweet/Like</h4>
     </div>
   </div>
-       
-    
-    `);
+    `;
+  return tweet;
 };
-
-// renderTweets(tweetData);
 
 $(function () {
   const $form = $("form");
@@ -64,7 +58,15 @@ $(function () {
       tweets
     ) {
       console.log("Success: ", tweets);
-      renderTweets(tweetData);
+      loadTweets();
     });
   });
 });
+
+const loadTweets = () => {
+  $.ajax("/tweets/", { method: "GET" }).then(function (tweets) {
+    renderTweets(tweets);
+  });
+};
+
+loadTweets();
