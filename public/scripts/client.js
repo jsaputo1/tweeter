575c51c2ad3d05ad84f03ada3f45284b4a91b425
John Saputo <jsaputo1@gmail.com>
2020-06-16 21:04:42 +0000

    added code to renderTweets func to rm preloaded tweets on post

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,36 +1,37 @@
-$(function () { //jQuery document ready function
+$(function () {
+  //jQuery document ready function
 
   // Default states
 
-  const error = $(".error h3")
+  const error = $(".error h3");
   error.hide();
 
   $(".new-tweet textarea").keyup(function () {
-    error.html("")
-    error.slideUp()
-  })
+    error.html("");
+    error.slideUp();
+  });
 
-  //Placeholder Tweets 
-  const tweetData = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-  ];
+  // //Placeholder Tweets
+  // const tweetData = [
+  //   {
+  //     user: {
+  //       name: "Newton",
+  //       avatars: "https://i.imgur.com/73hZDYK.png",
+  //       handle: "@SirIsaac",
+  //     },
+  //     content: {
+  //       text: "If I have seen further it is by standing on the shoulders of giants",
+  //     },
+  //     created_at: 1461116232227,
+  //   },
+  // ];
 
   // Functions
   const escape = function (str) {
-    let div = document.createElement('div');
+    let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-  }
+  };
 
   const createTweetElement = function (tweetData) {
     const tweet = `
@@ -50,56 +51,56 @@ $(function () { //jQuery document ready function
         </div>
       </div>
     `;
-    return tweet
+    return tweet;
   };
 
   const renderTweets = (tweetData) => {
+    $(".tweet").empty();
     for (const tweet of tweetData) {
       $(".tweet").prepend(createTweetElement(tweet));
     }
   };
 
   const loadTweets = () => {
-    $.ajax("/tweets/", { method: "GET" }).then(function (tweets) {
-      renderTweets(tweets);
+    $.ajax("/tweets/", {method: "GET"}).then(function (tweet) {
+      renderTweets(tweet);
     });
   };
 
   const errorCheck = function (tweetTextValue) {
     if (tweetTextValue.length === 0) {
-      error.html("Error: Tweet Cannot be Empty")
+      error.html("Error: Tweet Cannot be Empty");
       error.slideDown();
-      return error
-    }
-    else if (tweetTextValue.length > 140) {
-      error.html("Error: Exceeded max character limit")
+      return error;
+    } else if (tweetTextValue.length > 140) {
+      error.html("Error: Exceeded max character limit");
       error.slideDown();
-      return error
+      return error;
     }
-  }
+  };
 
-  loadTweets()
+  loadTweets();
   // renderTweets(tweetData)
 
   //Event listener and Ajax call
   const form = $("form");
   form.on("submit", function (event) {
     const queryString = $(this).serialize();
-    const tweetTextBox = $(this).find("#tweet-text")
-    const tweetTextValue = tweetTextBox.val().trim()
-    const counter = $(".counter")
+    const tweetTextBox = $(this).find("#tweet-text");
+    const tweetTextValue = tweetTextBox.val().trim();
+    const counter = $(".counter");
     event.preventDefault();
 
     if (errorCheck(tweetTextValue)) {
-      return error
+      return error;
     }
 
     $.ajax("/tweets/", {
       method: "POST",
       data: queryString,
     }).then(function () {
-      counter.html("140")
-      tweetTextBox.val("")
+      counter.html("140");
+      tweetTextBox.val("");
       loadTweets();
     });
   });
