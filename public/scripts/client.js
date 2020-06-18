e9476ddb5101652389e4259ae5cdb31854cd10a7
John Saputo <jsaputo1@gmail.com>
2020-06-18 17:51:59 +0000

    removed error from return statement on conditional

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,13 +2,12 @@ $(function () {
   //Load existing tweets in the database
   loadTweets();
   //Event listener and Ajax call
-  const error = $(".error h3");
   const form = $("form");
   form.on("submit", function (event) {
     const tweetTextValue = $(this).find("#tweet-text").val().trim();
     event.preventDefault();
     if (errorCheck(tweetTextValue)) {
-      return error;
+      return;
     }
     $.ajax("/tweets/", {
       method: "POST",
